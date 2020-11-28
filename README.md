0ddb36ea28c77eaa747de1610408cb3dfaf0bb63
rahiyansafz <rahiyansafin@gmail.com>
2020-11-29 01:09:22 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-read-more-c-6c6mct
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-read-more-c-6c6mct)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+    items = [
+    'On-brand but completeley fresh ladder up / ladder back to the strategy and we are running out of runway but drive awareness to increase engagement can we parallel path or products need full resourcing and support from a cross-functional team in order to be built, maintained, and evolved nor big picture. Circle back. Q1 are we in agreeance ladder up / ladder back to the strategy nor best practices paddle on both sides. Touch base land the plane can you ballpark the cost per unit for me. Work flows quick win meeting assassin, yet (lets not try to) boil the ocean (here/there/everywhere), usabiltiy, but out of the loop. Post launch reach out wiggle room, so we need to start advertising on social media. Hard stop. Churning anomalies organic growth. My capacity is full imagineer i am dead inside but move the needle not the long pole in my tent deep dive drink from the firehose. In an ideal world. Create spaces to explore whats next groom the backlog, guerrilla marketing accountable talk. Get six alpha pups in here for a focus group dunder mifflin.',
+  ];
+ }
